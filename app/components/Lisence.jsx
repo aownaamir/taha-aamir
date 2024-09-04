0f8d92b10a49ca1f9534ff5d2b41b9e87142b2ba
Aown-Aamir <aownaamirkips@gmail.com>
2024-09-04 19:52:40 +0500

    end

--- app/components/Lisence.jsx
+++ app/components/Lisence.jsx
@@ -2,13 +2,9 @@ function Lisence() {
   const year = new Date().getFullYear();
   return (
     <div className="w-full h-16 bg-slate-950 flex justify-center lg:justify-center items-center lg:px-20">
-      <p className=" text-slate-300 text-sm ">
-        {/* Taha Aamir © {year} &nbsp;|&nbsp; All rights reserved */}
+      <p className=" text-slate-300 text-xs ">
         All rights reserved © {year} &nbsp;|&nbsp; Design by Aown Aamir
       </p>
-      {/* <p className="hidden lg:block text-slate-300 text-sm">
-        Design by Aown Aamir
-      </p> */}
     </div>
   );
 }
